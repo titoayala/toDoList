7a6dfce9570f5fecc52a4d37950e465e10367c99
Oscar Ayala <titoayala@gmail.com>
2022-07-13 00:03:27 -0400

    Se agrega script de la guía de aprendizaje

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+//VARIABLES GLOBALES DE POSICIONAMIENTO EN EL HTML
+const inputTarea = document.querySelector("#tareaInput")
+const botonTarea = document.querySelector("#agregaTarea")
+const tablaTareas = document.querySelector("#tareas")
+const totalTareas = document.querySelector("#totalTareas")
+const tareasRealizadas = document.querySelector("#tareasRealizadas")
+
+//ARREGLO DE OBJETOS
+const misTareas = [
+    { id: 1, descripcion: "Maquetar la página" },
+    { id: 2, descripcion: "Agregar el JavaScript" },
+    { id: 3, descripcion: "Entregar el desafío" }
+]
+let idTarea = misTareas.length
+
+function renderTodasLasTareas() {
+    let html = ""
+    for (let tarea of misTareas) {
+        html += `<tr>
+                    <th>${tarea.id}</th>
+                    <td>${tarea.descripcion}</td>
+                    <td>NO</td>
+                    <td><button onclick="borrar(${tarea.id})"> x </button></li></td>
+                </tr>` 
+    }
+    tablaTareas.innerHTML = html //Actualiza el HTML
+    totalTareas.innerHTML = misTareas.length
+}
+
+botonTarea.addEventListener("click", () => {
+    const nuevaTarea = inputTarea.value //toma el texto ingresado
+    idTarea++
+    misTareas.push({ id: idTarea, descripcion: nuevaTarea }) //Se coloca el texto en el arreglo
+    renderTodasLasTareas()
+})
+
+function borrar(id) {
+    console.log(id)
+    const index = misTareas.findIndex(x => x.id == id)
+    misTareas.splice(index, 1)
+    renderTodasLasTareas()
+}
+renderTodasLasTareas()
